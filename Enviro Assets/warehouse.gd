52257a333e2d81175eff66f77d67e4259ef69768
logandalke <108908098+logandalke@users.noreply.github.com>
2022-10-24 23:09:28 -0500

    Breakable objects + enviro tweaks
    Breakable objects finished/polished. Still missing sound. Environment layout tweaks.

---
+++ Enviro Assets/warehouse.gd
@@ -0,0 +1,122 @@
+extends Node3D
+
+@onready var broken_glass = $brokenglass/CpuParticles3d
+@onready var broken_glass2 = $brokenglass2/CpuParticles3d
+@onready var broken_glass3 = $brokenglass3/CpuParticles3d
+@onready var broken_glass4 = $brokenglass4/CpuParticles3d
+@onready var broken_glass5 = $brokenglass5/CpuParticles3d
+@onready var broken_glass6 = $brokenglass6/CpuParticles3d
+@onready var broken_glass7 = $brokenglass7/CpuParticles3d
+@onready var broken_glass8 = $brokenglass8/CpuParticles3d
+@onready var broken_glass9 = $brokenglass9/CpuParticles3d
+@onready var broken_glass10 = $brokenglass10/CpuParticles3d
+@onready var broken_glass11 = $brokenglass11/CpuParticles3d
+@onready var broken_glass12 = $brokenglass12/CpuParticles3d
+@onready var broken_glass13 = $brokenglass13/CpuParticles3d
+@onready var broken_glass14 = $brokenglass14/CpuParticles3d
+@onready var broken_glass15 = $brokenglass15/CpuParticles3d
+@onready var broken_glass16 = $brokenglass16/CpuParticles3d
+@onready var broken_glass17 = $brokenglass17/CpuParticles3d
+@onready var broken_glass18 = $brokenglass18/CpuParticles3d
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_static_body_3d_1_tree_exited():
+	broken_glass.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_2_tree_exited():
+	broken_glass2.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_3_tree_exited():
+	broken_glass3.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_4_tree_exited():
+	broken_glass4.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_5_tree_exited():
+	broken_glass5.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_6_tree_exited():
+	broken_glass6.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_7_tree_exited():
+	broken_glass7.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_8_tree_exited():
+	broken_glass8.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_9_tree_exited():
+	broken_glass9.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_10_tree_exited():
+	broken_glass10.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_11_tree_exited():
+	broken_glass11.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_12_tree_exited():
+	broken_glass12.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_13_tree_exited():
+	broken_glass13.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_14_tree_exited():
+	broken_glass14.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_15_tree_exited():
+	broken_glass15.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_16_tree_exited():
+	broken_glass16.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_17_tree_exited():
+	broken_glass17.emitting = true
+	pass # Replace with function body.
+
+
+func _on_static_body_3d_18_tree_exited():
+	broken_glass18.emitting = true
+	pass # Replace with function body.
+
